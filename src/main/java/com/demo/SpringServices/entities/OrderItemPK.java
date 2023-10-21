7ccd44b7d6cbbcd98364110487fa03a3ee5a192f
DieguiinG <difejacyntho@hotmail.com>
2023-10-21 17:32:48 -0300

    OrderItem with many-to-many association

---
+++ src/main/java/com/demo/SpringServices/entities/OrderItemPK.java
@@ -0,0 +1,56 @@
+package com.demo.SpringServices.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product Product;
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Product getProduct() {
+		return Product;
+	}
+
+	public void setProduct(Product product) {
+		Product = product;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Product, order);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPK other = (OrderItemPK) obj;
+		return Objects.equals(Product, other.Product) && Objects.equals(order, other.order);
+	}
+
+}
